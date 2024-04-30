19e812534da1b72785744d701d8b3dbcd7d51a1a
NanaOdamme <eronastyles@gmail.com>
2024-04-30 08:11:33 +0000

    updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,21 +13,14 @@ def canUnlockAll(boxes):
         bool: True if all boxes can be opened, False otherwise.
     """
     if not boxes:
-        return False  # Empty boxes list, cannot unlock anything
-    
-    # Initialize a set to keep track of visited boxes
+        return False
     visited = set()
-    
-    # Initialize a list to keep track of boxes to be explored
-    to_explore = [0]  # Start with box 0
+    to_explore = [0]
     
     while to_explore:
-        current_box = to_explore.pop()  # Get the next box to explore
-        
-        # Mark the current box as visited
+        current_box = to_explore.pop()
         visited.add(current_box)
-        
-        # Add unvisited boxes to the list of boxes to explore
+
         for key in boxes[current_box]:
             if key not in visited and key not in to_explore and key < len(boxes):
                 to_explore.append(key)
